922abb3554cbea3a8c691e9bd7e5d215bbf0e31d
Samir Maciel <samirmaciel.dev@gmail.com>
2024-03-22 21:03:30 -0300

    setup colors

--- app/src/main/java/com/samirmaciel/gerenciadorbancoimobiliario/ui/theme/Color.kt
+++ app/src/main/java/com/samirmaciel/gerenciadorbancoimobiliario/ui/theme/Color.kt
@@ -2,9 +2,10 @@ package com.samirmaciel.gerenciadorbancoimobiliario.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val blue = Color(0xFF1A87DD)
+val dark_yellow = Color(0xFFF8BB18)
+val light_white = Color(0xFFF3F4F5)
+val light_blue = Color(0xFF3491DB)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)

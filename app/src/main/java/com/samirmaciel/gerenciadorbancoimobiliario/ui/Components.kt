098922451eb37f4fccb83bfae4c4c1ec6fa18e53
Samir Maciel <samirmaciel.dev@gmail.com>
2024-03-29 12:26:26 -0300

    refactor initialscreen

--- app/src/main/java/com/samirmaciel/gerenciadorbancoimobiliario/ui/Components.kt
+++ app/src/main/java/com/samirmaciel/gerenciadorbancoimobiliario/ui/Components.kt
@@ -1,15 +1,21 @@
 package com.samirmaciel.gerenciadorbancoimobiliario.ui
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -27,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -44,6 +51,7 @@ import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
+import com.samirmaciel.gerenciadorbancoimobiliario.ui.theme.blue
 import com.samirmaciel.gerenciadorbancoimobiliario.ui.theme.light_black
 
 
@@ -201,6 +209,48 @@ fun CustomTopBar(title: String, navController: NavController) {
                 text = title,
                 color = Color.Black,
                 style = SfProRoundedTypography.titleMedium
+            )
+        }
+    }
+}
+
+@SuppressLint("MissingPermission")
+@Composable
+fun BluetoothConnectionList(
+    deviceList: List<BluetoothDevice>,
+    onClick: (BluetoothDevice) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.padding(16.dp), verticalArrangement = Arrangement.Top
+    ) {
+       items(deviceList) {device ->
+           Row {
+               Text(
+                   text = device.name,
+                   modifier = Modifier
+                       .clickable { onClick(device) }
+                       .padding(16.dp)
+               )
+           }
+       }
+    }
+}
+
+@Composable
+fun DeviceConnectionCard(device: BluetoothDevice, onClickConnection: (BluetoothDevice) -> Unit){
+    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Text(modifier = Modifier.weight(1f), text = device.name, style = SfProRoundedTypography.titleMedium.copy(color = Color.Black))
+        IconButton(modifier = Modifier
+            .background(blue, RoundedCornerShape(5.dp))
+            .height(40.dp)
+            .width(40.dp), colors = IconButtonDefaults.iconButtonColors(
+            contentColor = Color.Black
+        ), onClick = { onClickConnection(device) }) {
+            Icon(
+                modifier = Modifier.padding(0.dp).rotate(90f),
+                painter = painterResource(id = R.drawable.send_icon),
+                contentDescription = "Connect bluetooth"
             )
         }
     }
